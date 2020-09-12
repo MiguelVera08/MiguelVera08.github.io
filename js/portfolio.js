489d954a75a1869bfd368409f6b515c26b088e7e
Miguel Vera <miguel.vera7908@gmail.com>
2020-09-11 21:55:23 -0500

    Removed extra margin added by accident to navbar

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,34 @@
 // jQuery to hide and fadein navbar
-(function($) {
+
+
+// (function($) {
+//     $(document).ready(function(){
+//         $(window).scroll(function(){
+//             if ($(this).scrollTop() > 200) {
+//                 $('.navbar').fadeIn(500);
+//             } else {
+//                 $('.navbar').fadeOut(500);
+//             }
+//         });
+//     });
+// })(jQuery);
+
+(function ($) {
     $(document).ready(function(){
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 200) {
-                $('.navbar').fadeIn(500);
-            } else {
-                $('.navbar').fadeOut(500);
-            }
+        // hide .navbar first
+        $(".navbar").hide();
+
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+                // set distance user needs to scroll before we fadeIn navbar
+                if ($(this).scrollTop() > 100) {
+                    $('.navbar').fadeIn();
+                } else {
+                    $('.navbar').fadeOut();
+                }
+            });
+
         });
     });
-})(jQuery);
+}(jQuery));
