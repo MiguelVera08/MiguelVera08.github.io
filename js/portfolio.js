7feb1260d7e7123663505f3e50597d0ff2aaae0f
Miguel Vera <miguel.vera7908@gmail.com>
2020-09-11 22:15:00 -0500

    Added headings to each experience column

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,34 +1,14 @@
 // jQuery to hide and fadein navbar
 
 
-// (function($) {
-//     $(document).ready(function(){
-//         $(window).scroll(function(){
-//             if ($(this).scrollTop() > 200) {
-//                 $('.navbar').fadeIn(500);
-//             } else {
-//                 $('.navbar').fadeOut(500);
-//             }
-//         });
-//     });
-// })(jQuery);
-
-(function ($) {
+(function($) {
     $(document).ready(function(){
-        // hide .navbar first
-        $(".navbar").hide();
-
-        // fade in .navbar
-        $(function () {
-            $(window).scroll(function () {
-                // set distance user needs to scroll before we fadeIn navbar
-                if ($(this).scrollTop() > 100) {
-                    $('.navbar').fadeIn();
-                } else {
-                    $('.navbar').fadeOut();
-                }
-            });
-
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 300) {
+                $('.navbar').fadeIn(500);
+            } else {
+                $('.navbar').fadeOut(500);
+            }
         });
     });
-}(jQuery));
+})(jQuery);
