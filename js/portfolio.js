c5bc331fa0515c70bdd0181bf38972a07733e233
Miguel Vera <miguel.vera7908@gmail.com>
2020-09-13 01:03:29 -0500

    Reverted js from lfs

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1597793c7c303356e7b4da4b1c229b4a29abb807948067b917372b1b7c4f4f84
-size 320
+// jQuery to hide and fadein navbar
+
+
+(function($) {
+    $(document).ready(function(){
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 300) {
+                $('.navbar').fadeIn(500);
+            } else {
+                $('.navbar').fadeOut(500);
+            }
+        });
+    });
+})(jQuery);
